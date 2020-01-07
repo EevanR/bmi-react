7cab8b4e39bb78ed8457ece1d89ded93bf0c1282
Eevan <eevan.redon@gmail.com>
2020-01-07 11:27:49 +0100

    cypress install

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Form from "./components/Form";
+import Message from "./components/Message";
+import { calculateBmi } from "./helper/bmiHelper";
+// import './App.css';
 
 class App extends Component {
+  state = {
+    weight: "",
+    height: "",
+    bmiValue: "",
+    bmiMessage: ""
+  };
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmitHandler = e => {
+    e.preventDefault();
+    const [bmiValue, bmiMessage] = calculateBmi(
+      this.state.weight,
+      this.state.height
+    );
+    this.setState({ bmiValue: bmiValue, bmiMessage: bmiMessage });
+  };
+
   render() {
     return (
-      <form>
-        <label htmlFor="weight">Weight</label>
-        <input
-          type="number"
-          required
-          placeholder="Weight in kgs"
-          name="weight"
-          id="weight"
+    <div>
+      <Form 
+        weight={this.state.weight}
+        height={this.state.height}
+        onChangeHandler={this.onChangeHandler}
+        onSubmitHandler={this.onSubmitHandler}
+      />
+      {this.state.bmiValue && (
+        <Message
+          bmiValue={this.state.bmiValue}
+          bmiMessage={this.state.bmiMessage}
         />
-        <label htmlFor="height">Height</label>
-        <input
-          type="number"
-          required
-          placeholder="Height in cm"
-          name="height"
-          id="height"
-        />
-        <button>Calculate BMI</button>
-      </form>
+      )}
+    </div>
     )
   }
 }
